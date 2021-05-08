dce0975c88ee933319a99bef6fee07bce0b35ae6
Emulie Chhor <emulie.chhor@hotmail.com>
2021-05-08 10:42:30 -0400

    Fixed #7: removed old symlinks before generating new symlinks

--- install.sh
+++ install.sh
@@ -5,22 +5,15 @@
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # ln -s ~/.dotfiles/.vim/autoload/plug.vim ~/.vim/autoload/plug.vim
 
-# remove old links
-
 # generate symbolic links in $HOME directory
 mkdir -p ~/.vim/init
-ln -s ~/dotfiles/.vim/init/apps.vim ~/.vim/init/apps.vim
-ln -s ~/dotfiles/.vim/init/colors.vim ~/.vim/init/colors.vim
-ln -s ~/dotfiles/.vim/init/coc.vim ~/.vim/init/coc.vim
-ln -s ~/dotfiles/.vim/init/fzf.vim ~/.vim/init/fzf.vim
-ln -s ~/dotfiles/.vim/init/functions.vim ~/.vim/init/functions.vim
-ln -s ~/dotfiles/.vim/init/fzf.vim ~/.vim/init/fzf.vim
-ln -s ~/dotfiles/.vim/init/git.vim ~/.vim/init/git.vim
-ln -s ~/dotfiles/.vim/init/mappings.vim ~/.vim/init/mappings.vim
-ln -s ~/dotfiles/.vim/init/snippets.vim ~/.vim/init/snippets.vim
-# ln -s ~/dotfiles/.vim/init/telescope.vim ~/.vim/init/telescope.vim
-ln -s ~/dotfiles/.vim/init/tree.vim ~/.vim/init/tree.vim
-ln -s ~/dotfiles/.vim/init/utils.vim ~/.vim/init/utils.vim
-ln -s ~/dotfiles/.vim/init/vimspector.vim ~/.vim/init/vimspector.vim
+for f in $(ls $HOME/.vim/init); do
+    unlink $HOME/.vim/init/$f
+    rm -rf $f
+    ln -s ~/dotfiles/.vim/init/$f ~/.vim/init/$f
+done
+
+# generate symbolic link for .vimrc
+unlink $HOME/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
